26cd6eebaf83326761b682353121002a807462b2
oreoluwa2013 <akeemdayo@gmail.com>
2026-03-21 13:39:55 +0000

    v1

--- act2.py
+++ act2.py
@@ -1,23 +1,7 @@
-def myfunction2(n):
-    # Base case
-    if (n <= 1):
-        return
-    
-    # Constant operation
-    print("Codingal")
-    
-    # Recursive call
-    myfunction2(n-1)
+binary = input("Enter a binary number: ")
 
-
-# Example function call
-myfunction2(5)
-
-# Recurrence Relation:
-# Each call performs O(1) work (print statement)
-# and then calls the function with (n-1)
-
-# T(n) = T(n-1) + O(1)
-
-# Solving the recurrence:
-# T(n) = O(n)
+try:
+    decimal = int(binary, 2)
+    print("Decimal:", decimal)
+except ValueError:
+    print("Invalid binary number")
