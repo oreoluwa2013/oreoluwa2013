2f034744bc96bdc052083ed427e5efa66f1fed3a
oreoluwa2013 <akeemdayo@gmail.com>
2026-03-21 13:43:52 +0000

    v1

--- act2.py
+++ act2.py
@@ -1,7 +1,13 @@
-binary = input("Enter a binary number: ")
+# Input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-try:
-    decimal = int(binary, 2)
-    print("Decimal:", decimal)
-except ValueError:
-    print("Invalid binary number")
+# Find the maximum
+max_num = max(num1, num2)
+
+# Loop until LCM is found
+while True:
+    if max_num % num1 == 0 and max_num % num2 == 0:
+        print("LCM:", max_num)
+        break
+    max_num += 1
