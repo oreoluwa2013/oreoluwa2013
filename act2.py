56188891d831e914aab724a292eacfffa4d69106
oreoluwa2013 <akeemdayo@gmail.com>
2026-03-28 16:16:45 +0000

    v1

--- act2.py
+++ act2.py
@@ -1,10 +1,20 @@
-def rightmost_set_bit(n):
-    if n == 0:
-        return 0
-    return n & -n
+def reverse_bits(n, bit_size=32):
+    reversed_num = 0
+    
+    for i in range(bit_size):
+        # Shift reversed_num left
+        reversed_num <<= 1
+        
+        # Add the last bit of n
+        reversed_num |= (n & 1)
+        
+        # Shift n right
+        n >>= 1
+    
+    return reversed_num
 
 # Example usage
 num = int(input("Enter a number: "))
-result = rightmost_set_bit(num)
+result = reverse_bits(num)
 
-print("Rightmost set bit value:", result)
+print("Reversed bits number:", result)
